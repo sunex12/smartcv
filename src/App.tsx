82a3b495b7f1c5aadb76a572428f23b32ab05879
Andris Potapenko <andrispotapenko@gmail.com>
2025-05-08 22:32:57 +0300

    vvv

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,20 @@ import { UploadResumePay } from "./pages/UploadResumePay";
 import { AnalysisResume } from "./pages/AnalysisResume";
 import { AnalysisResumePay } from "./pages/AnalysisResumePay";
 
-
 export const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route path="/" element={<Homepage />} index />
-          <Route path="/instruction" element={<Instruction />} />
-          <Route path="/upload-resume" element={<UploadResume />} />
-          <Route path="/upload-resume-payment" element={<UploadResumePay />} />
-          <Route path="/analysis-resume" element={<AnalysisResume />} />
-          <Route path="/analysis-resume-payment" element={<AnalysisResumePay />} />
+          <Route index element={<Homepage />} />
+          <Route path="instruction" element={<Instruction />} />
+          <Route path="upload-resume" element={<UploadResume />} />
+          <Route path="upload-resume-payment" element={<UploadResumePay />} />
+          <Route path="analysis-resume" element={<AnalysisResume />} />
+          <Route
+            path="analysis-resume-payment"
+            element={<AnalysisResumePay />}
+          />
         </Route>
       </Routes>
     </>
